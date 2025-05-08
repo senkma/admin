d42992c7210d174502baa0ae7476955ca5703305
senkma <senkyrik.marek@gmail.com>
2025-05-08 17:35:45 +0200

    Proste commit

---
+++ migrations/Version20250508153417.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250508153417 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE client ADD user_id INT NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE client ADD CONSTRAINT FK_C7440455A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_C7440455A76ED395 ON client (user_id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE client DROP FOREIGN KEY FK_C7440455A76ED395
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX IDX_C7440455A76ED395 ON client
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE client DROP user_id
+        SQL);
+    }
+}
